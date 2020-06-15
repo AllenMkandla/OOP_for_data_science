8eafaf0397ecc5f0511f19b1a762778c2e8b5195
Allen <allen.mkandla@umuzi.org>
2020-06-15 18:51:44 +0200

    plotter

---
+++ HistogramPlotter.py
@@ -0,0 +1,31 @@
+class Plotter():
+    def __init__(self,y_test,y_predicted):
+        self.y_test = y_test
+        self.y_predict = y_predicted
+
+    def run_calculations(self):
+        return self.y_test - self.y_predicted
+
+    def plot(self):
+        plt.hist(self.y_test - self.y_predicted)
+        plt.title('Residuals Plot for predictions')
+        plt.xlabel('Residuals')
+        plt.ylabel('Frequency')
+        return plt.show()
+#
+class HistogramPlotter(Plotter):
+    def __init__(self, y_test,y_predicted):
+        Plotter.__init__(self, y_test, y_predicted)
+
+class ScatterPlotter(Plotter):
+    def __init__(self, y_test, y_predicted):
+        Plotter.__init__(self, y_test, y_predicted)
+
+    def plot(self):
+        chart = pd.DataFrame({"y_test": self.y_test, "y_prediction": self.y_predicted})
+        chart.plot.scatter(x="y_test", y="y_predicted" , c="Blue")
+        plt.xlabel("Actual")
+        plt.title("Predicted vs Actual values")
+        plt.xlabel("Actual")
+        plt.ylabel("Prediction")
+        return plt.show()
